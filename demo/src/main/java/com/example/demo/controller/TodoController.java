0846f8a8a58765812d0881e73735b1cfe734b7f5
tyhan02 <tyhan02@naver.com>
2024-11-22 00:50:39 +0900

    commit_5

---
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.dto.ResponseDto;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo(){
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDto<String> response = ResponseDto.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+
+    }
+}
