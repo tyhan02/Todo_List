852c2f4eceb9b3e258e07ae048834e6843e3d6dd
tyhan02 <tyhan02@naver.com>
2024-11-21 23:53:12 +0900

    commit_4

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,20 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDto;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test") //리소스
 
 
 public class TestController {
-    @GetMapping("/{id}")
-    public String testControllerWithPathVariables(@PathVariable(required = false) int id)
-    {
-      return "Hello World id" + id;
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO
+    ) {
+        return "Hello World Id" + testRequestBodyDTO.getId() + "message " +
+                testRequestBodyDTO.getMessage();
     }
 }
 
