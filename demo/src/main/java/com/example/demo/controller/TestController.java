67d8730311e2ed962e23b4ed37de1dd150636b62
tyhan02 <tyhan02@naver.com>
2024-11-21 23:19:56 +0900

    commit_3

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -3,15 +3,18 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("test") //리소스
 
 
 public class TestController {
-    @GetMapping
-    public String testController(){
-        return "Hello World";
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id)
+    {
+      return "Hello World id" + id;
     }
 }
 
