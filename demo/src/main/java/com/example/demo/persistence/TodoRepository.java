5c47746804fbe863f023bbf7fb53ea45f3b06274
tyhan02 <tyhan02@naver.com>
2024-11-26 23:27:17 +0900

    commit_7

--- demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.TodoEntity;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    List<TodoEntity> findByUserId(String userId);
 
 }
--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,12 @@
 package com.example.demo.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
-
+@Slf4j
 @Service
 public class TodoService {
 
