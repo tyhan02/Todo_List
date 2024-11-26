1315f7dfbb481aa24eb1500c25b6285132fcb8e1
tyhan02 <tyhan02@naver.com>
2024-11-26 23:21:06 +0900

    commit_6

--- demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,17 +1,27 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
-
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id; // 오브젝트 아이디
     private String userId;  // 오브젝트 생성한 유저 아이디
     private String title; // Todo 타이틀
