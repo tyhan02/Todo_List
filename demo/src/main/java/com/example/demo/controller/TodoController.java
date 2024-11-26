9a5bcd0e57a57f5dee02391e4fa21c0d7a87233c
tyhan02 <tyhan02@naver.com>
2024-11-27 00:28:06 +0900

    commit_11

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,20 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDto;
+import com.example.demo.dto.TodoDto;
+import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.dto.ResponseDto;
-import com.example.demo.dto.TestRequestBodyDTO;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -23,13 +25,43 @@ public class TodoController {
     @Autowired
     private TodoService service;
 
-    @GetMapping("/test")
+    /*@GetMapping("/test")
     public ResponseEntity<?> testTodo(){
         String str = service.testService();
         List<String> list = new ArrayList<>();
         list.add(str);
         ResponseDto<String> response = ResponseDto.<String>builder().data(list).build();
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(response); }
+    */
 
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody TodoDto dto){
+        try {
+            String temporaryUserId = "temporary-user"; // temporary user id.
+
+            //(1) TodoEntity로 변환
+            TodoEntity entity = TodoDto.todoEntity(dto);
+            //(2) id를 null로 초기화, 생성 당시 id가 없어야 하기 때문
+            entity.setId(null);
+            //(3) 임시 사용자 아이디 설정
+            entity.setUserId(temporaryUserId);
+            //(4) 서비스를 이용해 todo 엔티티 생성
+            List<TodoEntity> entities = service.create(entity);
+            //(5) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDto 리스트로 변환
+            List<TodoDto> dtos = entities.stream().map(TodoDto::new).collect(Collectors.toList());
+            //(6) 변환된 todo 리스트를 이용해 responsedto 초기화
+            ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().data(dtos).build();
+            //(7) ResponseDto를 리턴
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e){
+            //(8) 예외는 dto 대신 error에 메세지 넣어 리턴
+            String error = e.getMessage();
+            ResponseDto<TodoDto> response = ResponseDto.<TodoDto>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+
+
+        }
     }
+
+
 }
