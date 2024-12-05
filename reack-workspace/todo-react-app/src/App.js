f1258007aabd5e819d5d852b86cd430bf9274802
tyhan02 <tyhan02@naver.com>
2024-12-05 16:24:46 +0900

    commit_17

---
+++ reack-workspace/todo-react-app/src/App.js
@@ -0,0 +1,39 @@
+import Todo from './Todo';
+import AddTodo from "./AddTodo";
+import React from "react";
+import { Paper, List, Container} from "@material-ui/core";
+import './App.css';
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+          { id: "0", title: "Hello world 1", done: true },
+          { id: "1", title: "Hello world 2", done: false },
+          ],
+    };
+  }
+
+  render() {
+    var todoItems= this.state.items.length > 0 && (
+        <Paper style={{ margin: 16}}>
+          <List>
+            {this.state.items.map((item, idx) => (
+                <Todo item={item} key={item.id} />
+            ))}
+          </List>
+        </Paper>
+    );
+
+    return (
+        <div className="App">
+            <Container maxWidth="md">
+                <AddTodo />
+                <div className="TodoList">{todoItems}</div>
+            </Container>
+            </div>
+    );
+  }
+}
+
+export default App;
